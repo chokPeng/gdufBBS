0f00b06336072956ad0595cee4becaa15c57145d
chokPeng <161543233@m.gduf.edu.cn>
2020-03-12 23:55:50 +0800

    add exception handling

---
+++ src/main/java/com/robben/bbs/service/CollectionService.java
@@ -0,0 +1,1 @@
+package com.robben.bbs.service;import com.robben.bbs.pojo.Collection;import java.util.List;public interface CollectionService {    Integer saveCollection(Collection collection);    Integer deleteCollection(Long collectorId,Long postId);    List getCollection(Long collectorId);    List getCollectedPostIdList(Long collectorId);}
